5f92c7f203fa6d488ccf25c07c32fd3c1efd6744
Matt <karbinex@gmail.com>
2021-02-17 15:15:26 -0800

    Added linear layout and listview

---
+++ app/src/main/java/com/example/comp3717assignment1/PlayerViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.comp3717assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player_view);
+    }
+}
